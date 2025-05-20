0a4630b0ab2454bbb3fd7aa1bf8b3779949d75cd
liachev <2000stan@gmail.com>
2025-05-20 16:58:47 +0500

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Header } from '../components/Header';
+import Header from '../components/Header';
 
 export default function HomePage() {
   return (
